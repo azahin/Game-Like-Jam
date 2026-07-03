84875bfec0355a88bb89a3d7cb6ceedb04c8957c
azahin <z4hin.4brar@gmail.com>
2026-07-02 20:27:39 -0600

    implement walking movement

--- smth_like/Assets/Scripts/Player/PlayerMovement.cs
+++ smth_like/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
-    [SerializeField] private Vector2 move;
-    [SerializeField] private Vector2 look;
+    private CharacterController controller;
 
+    [Header("Input References")]
     [SerializeField] private InputActionReference moveInput;
     [SerializeField] private InputActionReference lookInput;
+
+    [Header("Movement Variables")]
+    private Vector3 moveDirection;
+    [SerializeField] private float walkSpeed;
+
+    // --- Connections --- //
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
 
     void OnEnable() {
         moveInput.action.performed += HorizontalMovement;
@@ -22,19 +31,27 @@ public class PlayerMovement : MonoBehaviour {
         lookInput.action.canceled -= CancelLooking;
     }
 
+    // --- Movement Logic --- //
+    void Update() {
+        Vector3 velocity = walkSpeed * moveDirection;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
     private void HorizontalMovement(InputAction.CallbackContext ctx) {
-        move = ctx.ReadValue<Vector2>();
+        Vector2 rawInput = ctx.ReadValue<Vector2>();
+        moveDirection = transform.forward * rawInput.y + transform.right * rawInput.x;
+        moveDirection.y = 0.0f;
     }
 
     private void CancelMovement(InputAction.CallbackContext ctx) {
-        move = Vector2.zero;
+        moveDirection = Vector3.zero;
     }
 
     private void CameraMovement(InputAction.CallbackContext ctx) {
-        look = ctx.ReadValue<Vector2>();
+        // look = ctx.ReadValue<Vector2>();
     }
 
     private void CancelLooking(InputAction.CallbackContext ctx) {
-        look = Vector2.zero;
+        // look = Vector2.zero;
     }
 }
